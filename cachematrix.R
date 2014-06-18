52292a0a6d407a66fd4c5daaf08460e748903e5b
itziarAlbi <itziar_albi@hotmail.com>
2014-06-18 11:29:41 +0200

    Update cachematrix.R
    test if is a square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,27 @@
 
+
 ## Write a short comment describing this function
 
+
 makeCacheMatrix <- function(x = matrix()) {
+
 
 }
 
 
-#Calculates the inverse of a matrix, using the solve method, and assumming is a square matrix
+
+
+#Calculates the inverse of a matrix, using the solve method
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        solve(x);
+		#Is a square matrix ?, so the number of rows is equals to the number of columns
+		nRows <-nrow(x);
+		nCols<-ncol(x);
+		if (nRows == nCols){
+			solve(x);
+		}
+		
+        
 }
